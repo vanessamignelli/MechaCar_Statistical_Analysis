a0aa596fc171b710c6d0a81f3b8139a0b0894558
vanessamignelli <79024337+vanessamignelli@users.noreply.github.com>
2021-06-06 17:59:14 -0400

    adding challenge deliverables 1 and 2

---
+++ MechaCarChallenge.R
@@ -0,0 +1,23 @@
+#import packages
+library(tidyverse)
+library(dplyr)
+
+#import and read in the Mecha_car csv
+mechacar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#perform linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg)
+
+#determine p-value and r-squared value for the linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg))
+
+# Suspension Coil analysis 
+
+#import and read in the Suspension coil csv
+suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#create dataframe to get the mean, median, variance and STDEV of PSI column
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#create dataframe to group by lot and get summary statistics for each grouping
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
