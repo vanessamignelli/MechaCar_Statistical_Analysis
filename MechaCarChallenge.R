6d4bf7ac56183fb4584c50ddaa5ca84b574a8f14
vanessamignelli <79024337+vanessamignelli@users.noreply.github.com>
2021-06-06 18:15:26 -0400

    adding challenge deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,6 +2,8 @@
 library(tidyverse)
 library(dplyr)
 
+# Deliverable 1
+
 #import and read in the Mecha_car csv
 mechacar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
@@ -11,7 +13,7 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #determine p-value and r-squared value for the linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar_mpg))
 
-# Suspension Coil analysis 
+# Deliverable 2
 
 #import and read in the Suspension coil csv
 suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
@@ -21,3 +23,17 @@ total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI)
 
 #create dataframe to group by lot and get summary statistics for each grouping
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# Deliverable 3
+
+#t-test to determine if PSI across all manufacturing lots is statistically different from population mean
+t.test(suspension_coil$PSI,mu=1500)
+
+#t-test to determine if PSI of Lot 1 is statistically different from population mean
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
+#t-test to determine if PSI of Lot 2 is statistically different from population mean
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+
+#t-test to determine if PSI of Lot 3 is statistically different from population mean
+t.test(subset(suspension_coil, Manufacturing_Lot=="Lot3")$PSI,mu=1500)
